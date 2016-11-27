6d21ff87e30766ea966e6563e1d4dbeb66bce470
mbax3sm2 <struan.mcdonough@student.manchester.ac.uk>
2016-11-27 23:14:19 +0000

    Begin displaying explosions

--- dynamics/fireworks.cpp
+++ dynamics/fireworks.cpp
@@ -10,6 +10,17 @@
 | 2. Strange attactors
 | 3. A black hole
 */
+#define MAX_ROCKETS 10
+
+vector<particle> rockets;
+vector<particle> explosions;
+
+int numRockets = 0;
+int minRocketLifetime = 500;
+int maxRocketLifetime = 1000;
+int fireworkRadius = 20;
+int explodeLifetime = 400;
+
 void displayGrass() {
   for(unsigned int i = 0; i < grassSet.size(); i++) {
       glColor4ub(grassSet[i].r, grassSet[i].g, grassSet[i].b,
@@ -22,12 +33,125 @@ void displayGrass() {
 
 void displayEmitters() {
   for(unsigned int i = 0; i < emitterSet.size(); i++) {
+    if(emitterSet[i].lifetime == 0) {
       glColor4ub(emitterSet[i].r, emitterSet[i].g, emitterSet[i].b,
                  emitterSet[i].opacity);
       glVertex3f(emitterSet[i].initialX,
                  emitterSet[i].initialY,
                  emitterSet[i].initialZ);
+    } else {
+      glColor4ub(255, 0, 0,
+                 emitterSet[i].opacity);
+      glVertex3f(emitterSet[i].initialX,
+                 emitterSet[i].initialY,
+                 emitterSet[i].initialZ);
+      emitterSet[i].lifetime--;
+    }
   }
+}
+
+int getRandomEmitter(){
+  return (rand() % (int)(emitterSet.size() + 1));
+}
+
+int getRocketLifetime(){
+  return minRocketLifetime +
+    (rand() % (int)(maxRocketLifetime - minRocketLifetime + 1));
+}
+
+void createFireworkStream() {
+  if(numRockets < MAX_ROCKETS) {
+    GLint emitterNum = getRandomEmitter();
+    emitterSet[emitterNum].lifetime = emitterSet[emitterNum].maxLifetime;
+    particle emitter = emitterSet[emitterNum];
+    GLint lifetime = getRocketLifetime();
+
+    particle newParticle = {
+      emitter.initialX, emitter.initialY, emitter.initialZ,
+      0, 0.2, 0,
+      255.0, 255.0, 255.0,
+      255.0,
+      2.0,
+      5.0,
+      lifetime,
+      lifetime,
+      0
+    };
+
+    rockets.push_back(newParticle);
+    numRockets++;
+  }
+}
+
+void createExplode(particle origin)  {
+  for(int i = 0; i < 20; i++) {
+    GLfloat xDirection = (((explodeLifetime + 1) /2) * cos(i)) / 20;
+    GLfloat zDirection = (((explodeLifetime + 1) /2) * sin(i)) / 20;
+    GLfloat x = origin.initialX;
+    GLfloat y = origin.initialY;
+    GLfloat z = origin.initialZ;
+
+    particle newParticle = {
+      x, y, z,
+      ((xDirection+1) / 100), 0, ((zDirection+1) /100),
+      255.0, 0, 255.0,
+      255.0,
+      2.0,
+      5.0,
+      explodeLifetime,
+      explodeLifetime,
+      0
+    };
+
+    explosions.push_back(newParticle);
+  }
+}
+
+void drawExplode() {
+  glBegin(GL_POINTS);
+  for(unsigned int i = 0; i < explosions.size(); i++) {
+    if(explosions[i].lifetime == 0) {
+      explosions.erase(explosions.begin() + i);
+    } else {
+      explosions[i].lifetime--;
+      explosions[i].initialX += explosions[i].directionX;
+      explosions[i].initialZ += explosions[i].directionZ;
+
+      glColor4ub(explosions[i].r, explosions[i].g, explosions[i].b,
+        explosions[i].opacity);
+      glVertex3f(explosions[i].initialX,
+        explosions[i].initialY,
+        explosions[i].initialZ);
+    }
+  }
+  glEnd();
+}
+
+void drawFireworkStream() {
+  glBegin(GL_POINTS);
+
+  for(unsigned int i = 0; i < rockets.size(); i++) {
+    if(rockets[i].lifetime == 0) {
+      createExplode(rockets[i]);
+      rockets.erase(rockets.begin() + i);
+      numRockets--;
+    } else {
+      rockets[i].initialY += rockets[i].directionY;
+      rockets[i].lifetime--;
+      glColor4ub(rockets[i].r, rockets[i].g, rockets[i].b,
+        rockets[i].opacity);
+      glVertex3f(rockets[i].initialX,
+        rockets[i].initialY,
+        rockets[i].initialZ);
+    }
+  }
+
+  glEnd();
+}
+
+void renderFireWorkStream(){
+  createFireworkStream();
+  drawFireworkStream();
 }
 
 void fireworkDynamics() {
@@ -35,6 +159,8 @@ void fireworkDynamics() {
 
     displayGrass();
     displayEmitters();
+    renderFireWorkStream();
+    drawExplode();
 
   glEnd();
 
